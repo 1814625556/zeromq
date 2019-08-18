cc4724f412caa31414de6944d70e64d8f9a92dc7
chenchang <1814625556@qq.com>
2019-08-18 09:26:51 +0800

    精细探讨reqrep模式

---
+++ ReqRepDemo/DealerSample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace ReqRepDemo
+{
+    public class DealerSample
+    {
+        public static void AsyncSrv_Client(ZContext context, int i)
+        {
+            using (var client = new ZSocket(context, ZSocketType.DEALER))
+            {
+                // Set identity to make tracing easier
+                client.Identity = Encoding.UTF8.GetBytes("CLIENT" + i);
+                // Connect
+                client.Connect("tcp://127.0.0.1:5570");
+
+                var poll = ZPollItem.CreateReceiver();
+
+                var requests = 0;
+                while (true)
+                {
+                    // Tick once per second, pulling in arriving messages
+                    ZError error;
+                    for (var centitick = 0; centitick < 100; ++centitick)
+                    {
+                        if (!client.PollIn(poll, out var incoming, out error, TimeSpan.FromMilliseconds(10)))
+                        {
+                            if (Equals(error, ZError.EAGAIN))
+                            {
+                                Thread.Sleep(1);
+                                continue;
+                            }
+                            if (Equals(error, ZError.ETERM))
+                                return;    // Interrupted
+                            throw new ZException(error);
+                        }
+                        using (incoming)
+                        {
+                            var messageText = incoming[0].ReadString();
+                            Console.WriteLine("[CLIENT{0}] {1}", centitick, messageText);
+                        }
+                    }
+                    using (var outgoing = new ZMessage())
+                    {
+                        outgoing.Add(new ZFrame(client.Identity));
+                        outgoing.Add(new ZFrame("request " + (++requests)));
+
+                        if (client.Send(outgoing, out error)) continue;
+                        if (Equals(error, ZError.ETERM))
+                            return;    // Interrupted
+                        throw new ZException(error);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ReqRepDemo/RepSample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace ReqRepDemo
+{
+    public class RepSample
+    {
+        /// <summary>
+        /// 这是基础模式
+        /// </summary>
+        /// <param name="args"></param>
+        public static void HwServer0(string[] args)
+        {
+            // Create
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://127.0.0.1:5555");
+
+                //responder.Send(new ZFrame("chenchang"));//这里会报错，因为req套接字只能先receive才能发送
+
+                while (true)
+                {
+                    // Receive
+                    using (ZFrame request = responder.ReceiveFrame())
+                    {
+                        Console.WriteLine("Received {0}", request.ReadString());
+
+                        // Do some work
+                        Thread.Sleep(1);
+
+                        // Send
+                        responder.Send(new ZFrame("World"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 多帧模式--确实只会得到一帧
+        /// </summary>
+        /// <param name="args"></param>
+        public static void HwServer1(ZContext context)
+        {
+            // Create
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://127.0.0.1:5555");
+
+                while (true)
+                {
+                    // Receive
+                    using (var request = responder.ReceiveMessage())
+                    {
+                        Console.WriteLine("==================HwServer1 receive==================");
+                        for (var i = 0; i < request.Count; i++)
+                        {
+                            Console.WriteLine($"{i}:{request[i].ReadString()}");
+                        }
+                        Console.WriteLine("==================HwServer1 receive==================");
+                        // Do some work
+                        Thread.Sleep(1);
+
+                        // Send
+                        responder.Send(new ZFrame("World"));
+                    }
+                }
+            }
+        }
+    }
+}
